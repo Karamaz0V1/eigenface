88689dce4d2d82859ed5ab742561ebdd6edcf74f
Karamaz0v1 <florent.guiotte@gmail.com>
2015-11-18 17:07:48 +0100

    read pic

--- main.cpp
+++ main.cpp
@@ -6,14 +6,27 @@
  *
  **************************************************************************/
 
-#include <stdio.h>
+#include <iostream>
+#include <visp/vpImageIo.h>
+using namespace std;
+
+void load();
 
 int main( int argc, char* argv[] )
 {
     int ret = 0;
     
+    load();
 
     return ret;
 }
 
+void load() {
+    string fileURL = "../s1/1.pgm";
+    vpImage<unsigned char> im;
+    vpImageIo::readPGM(im, fileURL);
+    cout << im.getRows() << endl;
+    cout << im.getCols() << endl;
+
+}
 
