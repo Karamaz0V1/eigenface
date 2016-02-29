8c542350f23bd9e2bd3d411ac6f6b4b172c5ec9a
Karamaz0V1 <florent.guiotte@gmail.com>
2016-03-01 00:37:15 +0100

    Finally...

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
  *
  **************************************************************************/
 
-//#include <iostream>
 #include <visp/vpImageIo.h>
 #include <visp/vpDisplayX.h>
 #include <visp3/gui/vpPlot.h>
@@ -17,6 +16,8 @@ using namespace std;
 
 void load();
 void loadImage(vpImage<unsigned char> & I, int visage, int image);
+void test_visp();
+bool visp_broken();
 void demo_visp_broken();
 void demo_visp_broken2();
 
@@ -35,10 +36,11 @@ const int SUBJECTS = 36;
 const int IMAGES = 9;
 //const int SUBJECTS = 10;
 //const int IMAGES = 10;
-Eigenfaces ef("../img", SUBJECTS, IMAGES);
+Eigenfaces ef(IMGDIR, SUBJECTS, IMAGES);
 
 int main( int argc, char* argv[] )
 {
+    test_visp();
     q0();
     q3();
     q6();
@@ -379,8 +381,27 @@ void q15() {
         }
     }
 
-    //A.plot(0, 0, i, sum);
     A.getPixelValue(true);
+}
+
+void test_visp() {
+    if (visp_broken()) {
+        cout << "ERROR: you are using a buggy ViSP! DON'T PANIC and upgrade to ViSP 3.0.1" << endl;
+        exit(2);
+    }
+}
+
+bool visp_broken() {
+    int var;
+    vpMatrix mat(3, 4);
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            mat[i][j] = ++var;
+
+    vpRowVector row = mat.stackRows();
+    vpMatrix remat = row.reshape(3, 4);
+
+    return mat[2][3] != remat[2][3];
 }
 
 void demo_visp_broken() {
@@ -419,70 +440,3 @@ void demo_visp_broken2() {
 
     cout << retest << endl;
 }
-
-void load() {
-    int nbFaces = 40;
-    int nbPicsPerFace = 5;
-
-    // Load files specs
-    vpImage<unsigned char> im;
-    loadImage(im, 1, 1);
-    int iheight = im.getHeight();
-    int iwidth = im.getWidth();
-
-    // Build matrix
-    vpMatrix I(iheight * iwidth, nbPicsPerFace * nbFaces);
-
-    // Load matrix
-    for (int f = 0; f < nbFaces; f++) 
-        for (int pf = 0; pf < nbPicsPerFace; pf++) {
-            loadImage(im, f + 1, pf + 1);
-            for (int j = 0; j < iwidth; j++)
-                for (int i = 0; i < iheight; i++)
-                    I[i * iwidth + j][f * nbPicsPerFace + pf] = im(i, j) / 255.0;
-        }
-
-    // Disp matrix
-    //cout << I ;
-
-    // Compute mean face
-    vpColVector mean(iheight * iwidth);
-    for (unsigned int face = 0; face < I.getCols(); face++) 
-        for (int i = 0; i < iheight * iwidth; i++)
-            mean[i] += I[i][face];
-
-    mean /= nbFaces * nbPicsPerFace;
-
-    // Disp mean face
-    for (int i = 0; i < iheight; i++)
-        for (int j = 0; j < iwidth; j++)
-            im[i][j] = mean[i*iwidth+j] * 255;
-
-    vpDisplayX disp(im, 1000, 100, "mean face");
-    vpDisplay::display(im);
-    vpDisplay::flush(im);
-
-    vpMatrix imat;
-    vpImageToVpMatrix(im, imat);
-
-    // Face center
-    vpImageIo::writePGM(im, "moy.pgm");
-    vpImage<unsigned char> im40_1, im40_1c;
-    loadImage(im40_1, 40, 1);
-    //vpMatrix c127(iwidth, iheight, 127);
-    im40_1c = im40_1 - im;// + c127;
-    vpDisplayX disp0(im40_1, 1000 + iwidth, 100, "face");
-    vpDisplay::display(im40_1);
-    vpDisplay::flush(im40_1);
-    vpDisplayX disp1(im40_1c, 1000 + 2 * iwidth, 100, "center face");
-    vpDisplay::display(im40_1c);
-    vpDisplay::flush(im40_1c);
-    vpDisplay::getClick(im);
-}
-
-void loadImage(vpImage<unsigned char> & I, int visage, int image) {
-    stringstream fileURL;
-    fileURL << IMGDIR << "s" << visage << "/" << image << ".pgm";
-    vpImageIo::readPGM(I, fileURL.str());
-}
-
